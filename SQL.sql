5013008f802605211dd211c4f8266534a3c98b96
Alex84 <fantom.rpg@mail.ru>
2024-05-22 12:16:10 +0300

    DZ

--- SQL.sql
+++ SQL.sql
@@ -3,46 +3,47 @@ use SQL;
 
 -- 1.	Создайте функцию, которая принимает кол-во сек и форматирует их в кол-во дней, часов, минут и секунд.
 -- Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
-
-DELIMITER //
-CREATE PROCEDURE second_counter(num INT)
+ CREATE FUNCTION format_seconds(seconds INT)
+RETURNS VARCHAR(255)
+DETERMINISTIC
 BEGIN
-	CASE
-		WHEN num < 60 THEN
-			SELECT CONCAT(num, ' ', 'seconds') AS Result;
-        WHEN num >= 60 AND num < 3600 THEN
-			SELECT CONCAT_WS(' ', num DIV 60, 'minutes', MOD(num, 60), 'seconds') AS Result;
-        WHEN num >= 3600 AND num < 86400 THEN
-			SELECT CONCAT_WS(' ', num DIV 3600, 'hours', MOD(num, 3600) DIV 60, 'minutes', MOD(MOD(num, 3600),60), 'seconds') AS Result;
-        ELSE
-			SELECT CONCAT_WS(' ', num DIV 86400, 'days', MOD(num, 86400) DIV 3600, 'hours', MOD(MOD(num, 86400),3600) DIV 60, 'minutes',
-                             MOD(MOD(MOD(num, 86400),3600),60), 'seconds') AS Result;
-    END CASE;
-END//
+  DECLARE days INT;
+  DECLARE hours INT;
+  DECLARE minutes INT;
+  DECLARE seconds_remaining INT;
 
-DELIMITER ;
-CALL second_counter(123456);  
+  SET days = seconds DIV 86400;
+  SET hours = (seconds MOD 86400) DIV 3600;
+  SET minutes = (seconds MOD 3600) DIV 60;
+  SET seconds_remaining = seconds MOD 60;
+
+  RETURN CONCAT(days, ' days ', hours, ' hours ', minutes, ' minutes ', seconds_remaining, ' seconds');
+END;
+
+CALL second_counter(123456);
+
 
 -- 2.	Выведите только четные числа от 1 до 10 включительно.
 -- Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)
 
-DELIMITER //
-CREATE PROCEDURE get_even(`start` INT, `end` INT)
-BEGIN
-	DECLARE i INT DEFAULT `start`;
-    DECLARE res_str TEXT DEFAULT NULL;
-    WHILE  i<=`end` DO
-        IF i%2 = 0 THEN
-			IF res_str IS NULL THEN
-				SET res_str = concat(i);
-			ELSE
-				SET res_str = concat(res_str, ', ', i);
-			END IF;
-		END IF;
-        SET i = i + 1;
-    END WHILE;
-	SELECT res_str;
-END //
-DELIMITER ;
+SELECT number
+FROM (
+  SELECT number
+  FROM (
+SELECT n AS number
+    FROM (
+      SELECT t.n
+      FROM (
+        SELECT ROW_NUMBER() OVER (ORDER BY n) AS n
+        FROM t
+      ) AS t
+    ) AS t
+    WHERE t.n <= 10
+  ) AS t
+  WHERE t.number % 2 = 0
+) AS t;
 
-CALL get_even(1, 10); 
+CALL get_even(1, 10);
+
+
+ 
